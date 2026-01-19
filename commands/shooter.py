9ad9cd574412cde787c8436bcf08afe0efadd470
Tyler <27shradert@brrice.edu>
2026-01-19 18:04:17 -0500

    saturday

--- commands/shooter.py
+++ commands/shooter.py
@@ -7,3 +7,30 @@ from oi.keymap import Keymap
 # from commands.wrist import Wrist
 
 from subsystems.shooter import Shooter 
+
+
+
+class Shoot(commands2.Command):
+    #This command used to run shooter motor
+    def __init__(
+        self, 
+        app: Shooter,
+    ) -> None:
+        super().__init__()
+
+        self.app = app
+        self.addRequirements(app)
+        
+    def initialize(self) -> None:
+        self.finished = False
+
+    def execute(self) -> None:
+        self.finished = False
+        self.app.shooterOn(1) #Intake setIntakeSpeed in subsystem is function to actually set the intake speed
+
+    def isFinished(self) -> bool:
+        return self.finished
+        
+    def end(self, interrupted=False) -> None:
+
+        self.app.setShooterOffSpeed(0) #Intake setIntakeSpeed in subsystem is function to actually set the intake speed
--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -8,3 +8,26 @@ class Shooter(commands2.SubsystemBase):
 
     def __init__(self) -> None:
         super().__init__()
+        
+        self.m_shooter = rev.SparkMax(58, rev.SparkMax.MotorType.kBrushless)
+              
+    def setShooterOutSpeed(self, speed: float) -> bool:
+        '''Sets the speed of the intake motors.
+        
+        Args:
+            speed: The speed to set the motors to, -1 to 1.
+        '''
+        
+        self.m_shooter.set(speed)
+        #print("Shooter out Speed:", speed)
+
+    def setShooterOutSpeed(self, speed: float) -> bool:
+        #print("Shooter Out Speed:", speed)
+        self.m_shooter.set(speed)
+    
+    def TeleopSetShooterOutSpeed(self, speed: float) -> bool:
+        #print("Shooter Out Speed:", speed)
+        if config.current_ele_pos > -2:
+            self.m_shooter.set(speed * 0.5)
+        else:
+            self.m_shooter.set(speed)        
